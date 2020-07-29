c02062f4495094769aedd7143e2edb8f8a72aa13
Russ Cox <rsc@golang.org>
2020-07-29 03:21:35 -0400

    app/www: update to go114 runtime

--- app/www/main.go
+++ app/www/main.go
@@ -2,16 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package www
+package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"runtime"
 
 	"rsc.io/swtch/servegcs"
 )
 
-func init() {
+func main() {
+	http.HandleFunc("/.info", info)
 	http.Handle("/", servegcs.Handler("swtch.com", "swtch/www"))
 	http.HandleFunc("/plan9port/", servegcs.RedirectHost("9fans.github.io"))
 	http.HandleFunc("www.swtch.com/", servegcs.RedirectHost("swtch.com"))
+
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
+
+func info(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go version: %s\n", runtime.Version())
 }

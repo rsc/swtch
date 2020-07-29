807b1439529889d8f12fdf40bf95ea2c4034f726
Russ Cox <rsc@golang.org>
2020-07-28 23:18:09 -0400

    9fans-net: update to go114 runtime

---
+++ app/9fans-net/go.mod
@@ -0,0 +1,5 @@
+module rsc.io/swtch/app/9fans-net
+
+go 1.15
+
+require rsc.io/go-import-redirector v0.0.1
--- app/9fans-net/main.go
+++ app/9fans-net/main.go
@@ -2,19 +2,17 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package ninefansnet
+package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"runtime"
 
 	"rsc.io/go-import-redirector/godoc"
 )
-
-func init() {
-	http.HandleFunc("/", root)
-	http.HandleFunc("/archive/", archive)
-	http.Handle("/go/", godoc.Redirect("git", "9fans.net/go", "https://github.com/9fans/go"))
-}
 
 func root(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -22,7 +20,7 @@ func root(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, "http://plan9.bell-labs.com/plan9/", http.StatusFound)
+	http.Redirect(w, r, "http://9p.io/plan9/", http.StatusFound)
 }
 
 func archive(w http.ResponseWriter, r *http.Request) {
@@ -31,4 +29,16 @@ func archive(w http.ResponseWriter, r *http.Request) {
 
 <p>See <a href="https://9p.io/wiki/plan9/mailing_lists/">https://9p.io/wiki/plan9/mailing_lists/</a> for alternatives.
 `))
+}
+
+func main() {
+	http.HandleFunc("/.info", info)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/archive/", archive)
+	http.Handle("/go/", godoc.Redirect("git", "9fans.net/go", "https://github.com/9fans/go"))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
+
+func info(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go version: %s\n", runtime.Version())
 }
